376ab54cf47d1e56baf5f7ee30b8502ea3e12cd4
karabinata <karabinata1992@gmail.com>
2018-01-06 21:42:44 +0200

    Adding area routing

--- Semkovo/Semkovo.Api/Startup.cs
+++ Semkovo/Semkovo.Api/Startup.cs
@@ -55,8 +55,7 @@ namespace Semkovo.Web
                 options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
             });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseDatabaseMigration();
