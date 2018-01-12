7537f30e52f0d064c55133fa6802000a8c906e95
Elka Dimitrova <dimitrova_elka@abv.bg>
2018-01-12 23:19:21 +0200

    add Like articles button

---
+++ Semkovo/Semkovo.Api/Models/Likes/ArticleVoteViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Semkovo.Common.Mapping;
+using Semkovo.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+using static Semkovo.Data.DataConstants;
+
+namespace Semkovo.Web.Models.Articles
+{
+    public class ArticleVoteViewModel : IMapFrom<ArticleVote>
+    {
+        [Required]
+        public int Count { get; set; }
+        public int? ArticleId { get; set; }
+    }
+}
